262cc8cc89df79c637ee820523680002a6c0a4cb
Hoang04 <hoangvv07022004@gmail.com>
2023-10-10 16:19:31 +0700

    Study session 2

---
+++ ss2.playground/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+// 3. constants and variables
+
+let age = 12
+
+var ages = 13
+ages = 14
+ages = 15
+// 4. data type
+
+var myAge = 19
+var myName = "Hoang"
+var weight = 64.5
+var choose = true
+
+// 5.String data type
+
+var name = "Peter"
+name = "Lisa"
+let animal = "dog"
+let anotherFriend2: String
+anotherFriend2 = "jacky"
+let firstName = "Doe"
+let lastName = "Jonh"
+let fullName = firstName + lastName
+print(fullName)
+
+name.uppercased()
+name.lowercased()
+name.capitalized
+name.isEmpty
+name.hasPrefix("Pe")
+name.hasPrefix("Li")
+name.hasSuffix("sa")
+
+//6. int data type
+
+let x = 5_000_000
+let y = 6
+let sum = x + y
+
+//7. float and double data type
+
+var z = 5.0
+var a = 6.34
+var sum1 = z + a
+
+let fl:Float = 46565.5745443
+let doub:Double = 4656.5745443
+
+//9.String interpolation
+
+let firstname = "Han"
+let lastname = "John"
+let Age = 19
+
+"My name is \(firstname) \(lastname), I am \(Age) year olds."
+"I am \(Age + 2) years old."
+
+
+// 1:
+// Doe Jonh 19
+print("\(firstName) \(lastName) \(myAge)")
+// 2:
+// myAge + weight
+let myAgeDouble = Double(myAge)
+let Sum = myAgeDouble + weight
+
+
+
+
+
+
